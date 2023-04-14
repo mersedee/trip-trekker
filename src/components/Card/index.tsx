99abacc6fabc3b6502dfdba0777e87ac93df3ac8
mersedee <mersede.rahbar@gmail.com>
2023-04-14 13:43:01 +0200

    feat: add card component

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,27 @@
+import {type FC} from 'react'
+import sampleSrc from '@/assets/images/sample.png'
+
+const Card: FC = () => {
+  return (
+    <div className="max-w-sm bg-white border border-gray-50 rounded-xl shadow">
+      <a className="block pt-3 px-3" href="/">
+        <img className="rounded-xl" src={sampleSrc} alt="sample"/>
+      </a>
+
+      <div className="p-4">
+        <a href="/">
+          <h5 className="mb-2 text-xl font-bold tracking-tight">
+              Noteworthy technology acquisitions 2021
+          </h5>
+        </a>
+        <p className="mb-3 font-normal text-gray-700">
+            Here are the biggest enterprise technology
+            acquisitions of 2021 so far, in reverse chronological order.
+        </p>
+      </div>
+    </div>
+
+  )
+}
+
+export default Card
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,19 @@
 import { type FC } from 'react'
 import Header from '@/components/Header'
+import Card from '@/components/Card'
 
 const Home: FC = () => {
   return (
-    <div>
+    <>
       <Header />
-     Home page
-    </div>
+      <div className="grid grid-cols-2 px-4 py-8">
+        <div className="grid grid-cols-2 gap-6">
+            <Card />
+            <Card />
+        </div>
+        <div>map</div>
+      </div>
+    </>
   )
 }
 
