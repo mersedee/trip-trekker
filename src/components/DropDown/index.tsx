2c6751b0cb5e091574d3751d8dbef7d325a2ff53
mersedee <mersede.rahbar@gmail.com>
2023-04-15 17:52:55 +0200

    feat: add drop down component

---
+++ src/components/DropDown/index.tsx
@@ -0,0 +1,70 @@
+import { type FC, useState, useRef, useEffect } from 'react'
+import { ChevronDown } from 'react-feather'
+
+interface Menu {
+  label: string
+  value: string
+}
+
+interface Props {
+  menus: Menu[]
+  width?: number
+}
+
+const DropDown: FC<Props> = ({ menus, width }) => {
+  const [open, setOpen] = useState<boolean>(false)
+  const [selectedOption, setSelectedOption] = useState<Menu>(menus[0])
+  const ref = useRef<HTMLDivElement>(null)
+
+  const handleClickOutside = (event: any): void => {
+    const target = event.target as HTMLElement
+    if ((ref.current != null) && !ref.current.contains(target)) {
+      setOpen(false)
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  },
+  [ref])
+
+  const Toggle = (): void => { setOpen((prevOpen) => !prevOpen) }
+
+  const handleOptionClick = (option: Menu): void => {
+    setSelectedOption(option)
+    Toggle()
+  }
+
+  return (
+    <div ref={ref} className="relative w-fit">
+      <button
+        type="button"
+        style={{ width: `${width}px` }}
+        className="bg-white border border-gray-50 focus:outline-none font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex flex justify-between items-center"
+        onClick={Toggle}
+      >
+        {selectedOption.label}
+        <ChevronDown size={18} />
+      </button>
+
+      <div className={`absolute z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 ${open ? 'block' : 'hidden'}`}>
+        <ul className="py-2 text-sm text-gray-700">
+          {menus.map(menu => (
+            <li
+              key={menu.value}
+              className="block px-4 py-2 hover:bg-gray-50 cursor-pointer"
+              onClick={() => { handleOptionClick(menu) } }
+            >
+              {menu.label}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default DropDown
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,16 +2,27 @@ import { type FC } from 'react'
 import Header from '@/components/Header'
 import Card from '@/components/Card'
 import Map from '@/components/Map'
+import DropDown from '@/components/DropDown'
+
+const menus = [
+  { label: 'Restaurants', value: 'restaurant' },
+  { label: 'Hotels', value: 'hotels' },
+  { label: 'Attractions', value: 'attraction' }
+]
 
 const Home: FC = () => {
   return (
     <>
       <Header />
-      <div className="grid grid-cols-2 px-4 py-8">
-        <div className="grid grid-cols-2 gap-6 pr-8">
-          <Card />
-          <Card />
+      <div className="grid grid-cols-2 py-8 px-4">
+        <div>
+          <DropDown menus={menus} width={150} />
+          <div className="grid grid-cols-2 gap-6 pr-8 pt-4">
+            <Card />
+            <Card />
+          </div>
         </div>
+
         <div className="relative rounded-xl overflow-hidden"><Map /></div>
       </div>
     </>
